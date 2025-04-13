797847a531c2203c021c55e0a9781d6cb62ccd6d
Simon Waloschek <waloschek@pm.me>
2025-04-13 21:13:29 +0200

    Updated vite and fixed vite config

--- vite.config.ts
+++ vite.config.ts
@@ -2,13 +2,17 @@ import tailwindcss from "@tailwindcss/vite";
 import { defineConfig } from "vite";
 import solidPlugin from "vite-plugin-solid";
 
-export default defineConfig({
-	base: "./",
-	plugins: [solidPlugin(), tailwindcss()],
-	server: {
+export default defineConfig(({ command }) => {
+	const base = command === 'build' ? '/maria-anna-waloschek.de/' : '/'; // Conditional base path
+  
+	return {
+	  base: base, // Set the base dynamically
+	  plugins: [solidPlugin(), tailwindcss()],
+	  server: {
 		port: 3000,
-	},
-	build: {
-		target: "esnext",
-	},
-});
+	  },
+	  build: {
+		target: 'esnext',
+	  },
+	};
+  });
