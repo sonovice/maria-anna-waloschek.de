82679eb58d1d6f503cf419253c4f5d3b5a05d5aa
Simon Waloschek <waloschek@pm.me>
2025-05-04 21:51:22 +0200

    Fixed base path

--- vite.config.ts
+++ vite.config.ts
@@ -3,10 +3,10 @@ import { defineConfig } from "vite";
 import solidPlugin from "vite-plugin-solid";
 
 export default defineConfig(({ command }) => {
-	const base = command === 'build' ? '/maria-anna-waloschek.de/' : './'; // Conditional base path
+	// const base = command === 'build' ? '/maria-anna-waloschek.de/' : './'; // Conditional base path
   
 	return {
-	  base: base, // Set the base dynamically
+	  base: "./",
 	  plugins: [solidPlugin(), tailwindcss()],
 	  server: {
 		port: 3000,
